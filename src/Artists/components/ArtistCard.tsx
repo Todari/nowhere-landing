eaff6d29aa6261c9138c2ac543467551898511bf
이태훈 <rhymint@gmail.com>
2024-01-11 18:47:05 +0900

    feat: artistCard 클릭 시 artistInfo 보여주는 기능 추가

--- src/Artists/components/ArtistCard.tsx
+++ src/Artists/components/ArtistCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAppSelector } from "../../hooks";
 import { ReactComponent as INSTAGRAM } from "../asset/instagram_logo.svg";
 import { ReactComponent as SOUNDCLOUD } from "../asset/soundcloud_logo.svg";
@@ -13,11 +14,16 @@ type ArtistCardProps = {
 
 const ArtistCard = ({ id, name, profile, path, soundCloud, instagram }: ArtistCardProps) => {
   const { width } = useAppSelector((state) => state.screenSize)
+  const [infoOpened, setInfoOpened] = useState(false);
+
+  const handleInfoOpened = () => {
+    setInfoOpened(!infoOpened)
+  }
 
   return (
-    <div>
+    <div className="flex flex-col">
       {id % 2 !== 0 && width < 768 ?
-        <div className='flex flex-row justify-center'>
+        <div className='flex flex-row justify-center' onClick={handleInfoOpened}>
           <div className="grow flex flex-col items-center justify-center p-4">
             <div className="grow flex items-center">
               <div className="text-2xl text-white text-center">
@@ -25,15 +31,19 @@ const ArtistCard = ({ id, name, profile, path, soundCloud, instagram }: ArtistCa
               </div>
             </div>
             <div className="grow flex flex-row w-full items-center justify-center">
-              <INSTAGRAM className="grow" onClick={() => { window.open(instagram) }} />
-              <SOUNDCLOUD className="grow" onClick={() => { window.open(soundCloud) }} />
+              <div className="grow flex justify-center">
+                <INSTAGRAM onClick={(e) => { e.stopPropagation(); window.open(instagram); }} />
+              </div>
+              <div className="grow flex justify-center">
+                <SOUNDCLOUD onClick={(e) => { e.stopPropagation(); window.open(soundCloud) }} />
+              </div>
             </div>
           </div>
           <div className="w-full basis-1/2 aspect-square bg-stone-500">
           </div>
         </div>
         :
-        <div className='flex flex-row justify-center'>
+        <div className='flex flex-row justify-center' onClick={handleInfoOpened}>
           <div className="w-full basis-1/2 aspect-square bg-stone-500">
           </div>
           <div className="grow flex flex-col items-center justify-center p-4">
@@ -43,13 +53,25 @@ const ArtistCard = ({ id, name, profile, path, soundCloud, instagram }: ArtistCa
               </div>
             </div>
             <div className="grow flex flex-row w-full items-center justify-center">
-              <INSTAGRAM className="grow" onClick={() => { window.open(instagram) }} />
-              <SOUNDCLOUD className="grow" onClick={() => { window.open(soundCloud) }} />
+              <div className="grow flex justify-center">
+                <INSTAGRAM onClick={(e) => { e.stopPropagation(); window.open(instagram) }} />
+              </div>
+              <div className="grow flex justify-center">
+                <SOUNDCLOUD onClick={(e) => { e.stopPropagation(); window.open(soundCloud) }} />
+              </div>
             </div>
           </div>
         </div>
       }
-    </div>
+      {
+        infoOpened ?
+          <div className="h-200 bg-stone-800">
+            sss
+          </div>
+          :
+          null
+      }
+    </div >
 
   )
 }
