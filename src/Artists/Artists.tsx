303d3368a15d3c4629caaea0ffbf49a3ca6fcc0b
이태훈 <rhymint@gmail.com>
2024-01-11 18:24:51 +0900

    feat: artist 카드 instagram, soundcloud link 추가

--- src/Artists/Artists.tsx
+++ src/Artists/Artists.tsx
@@ -5,18 +5,18 @@ import ARTIST_LIST from "../constant/ArtistList";
 const Artists = () => {
   const { width } = useAppSelector((state) => state.screenSize);
   const artistList: JSX.Element[] = ARTIST_LIST.list.map(
-    (artist) => <ArtistCard key={artist.id} name={artist.name} path={''} profile={artist.profile} instagram={artist.instagram} soundCloud={artist.soundCloud} />
+    (artist) => <ArtistCard id={artist.id} name={artist.name} path={''} profile={artist.profile} instagram={artist.instagram} soundCloud={artist.soundCloud} />
   );
 
   return (
     <div className="bg-black p-12">
       {width < 768 ?
-        <div className="py-20 flex flex-col gap-8">
+        <div className="py-20 flex flex-col gap-16">
           {artistList}
         </div>
         :
         <div className="py-20">
-          <div className="grid grid-cols-2 gap-8">
+          <div className="grid grid-cols-2 gap-16">
             {artistList}
           </div>
         </div>}
--- src/Artists/components/ArtistCard.tsx
+++ src/Artists/components/ArtistCard.tsx
@@ -1,7 +1,9 @@
+import { useAppSelector } from "../../hooks";
 import { ReactComponent as INSTAGRAM } from "../asset/instagram_logo.svg";
 import { ReactComponent as SOUNDCLOUD } from "../asset/soundcloud_logo.svg";
 
 type ArtistCardProps = {
+  id: number
   name: string,
   profile: string,
   path: string,
@@ -9,19 +11,46 @@ type ArtistCardProps = {
   instagram: string,
 }
 
-const ArtistCard = ({name, profile, path, soundCloud, instagram} : ArtistCardProps) => {
+const ArtistCard = ({ id, name, profile, path, soundCloud, instagram }: ArtistCardProps) => {
+  const { width } = useAppSelector((state) => state.screenSize)
 
-  
   return (
-    <div className='flex flex-row justify-center border-2'>
-      <div className="w-full basis-1/2 aspect-square bg-stone-500">
-      </div>
-      <div className="grow flex items-center justify-center p-4">
-        <div className="text-3xl text-white text-center ">
-          {name}
+    <div>
+      {id % 2 !== 0 && width < 768 ?
+        <div className='flex flex-row justify-center'>
+          <div className="grow flex flex-col items-center justify-center p-4">
+            <div className="grow flex items-center">
+              <div className="text-2xl text-white text-center">
+                {name}
+              </div>
+            </div>
+            <div className="grow flex flex-row w-full items-center justify-center">
+              <INSTAGRAM className="grow" onClick={() => { window.open(instagram) }} />
+              <SOUNDCLOUD className="grow" onClick={() => { window.open(soundCloud) }} />
+            </div>
+          </div>
+          <div className="w-full basis-1/2 aspect-square bg-stone-500">
+          </div>
         </div>
-      </div>
+        :
+        <div className='flex flex-row justify-center'>
+          <div className="w-full basis-1/2 aspect-square bg-stone-500">
+          </div>
+          <div className="grow flex flex-col items-center justify-center p-4">
+            <div className="grow flex items-center">
+              <div className="text-2xl text-white text-center">
+                {name}
+              </div>
+            </div>
+            <div className="grow flex flex-row w-full items-center justify-center">
+              <INSTAGRAM className="grow" onClick={() => { window.open(instagram) }} />
+              <SOUNDCLOUD className="grow" onClick={() => { window.open(soundCloud) }} />
+            </div>
+          </div>
+        </div>
+      }
     </div>
+
   )
 }
 
