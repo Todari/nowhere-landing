474309ad70dcb26fd4185782de57db095923fe76
이태훈 <rhymint@gmail.com>
2024-01-11 19:50:37 +0900

    fix: header에 가려 상단 부분 click 이벤트 발생하지 않던 오류 수정

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -27,40 +27,42 @@ const Header = () => {
 
   useEffect(() => {
     if (showSubMenuDropdown) {
-      setSubMenuY(0)
+      setSubMenuY(64)
     } else {
       setSubMenuY(-240)
     }
   }, [showSubMenuDropdown])
 
   return (
-    <motion.div className="fixed flex w-full flex-col z-20"
-      animate={{ y: headerY }}
-      transition={{ duration: 0.3 }}>
-      <div className='top-0 w-full flex flex-row gap-4 h-16 justify-center items-center bg-black z-20'>
-        <div className='flex flex-row gap-4 h-16 items-center bg-black w-5/6'>
+    <div className="flex justify-center h-min shrink">
+      <motion.div className="fixed flex w-full flex-col z-20 h-min" onClick={() => { console.log("clicked") }}
+        animate={{ y: headerY }}
+        transition={{ duration: 0.3 }}>
+        <div className='top-0 w-full flex flex-row gap-4 h-16 justify-center items-center bg-black z-20'>
+          <div className='flex flex-row gap-4 h-16 items-center bg-black w-5/6'>
 
-          <div className='grow flex items-center justify-start'>
-            <motion.div className="" onClick={goHome}
-              whileTap={{
-                scale: 0.95,
-                transition: { duration: 0.1 }
-              }}>
-              <NOWHERE_LOGO className="h-10" />
-            </motion.div>
-          </div>
-          {width >= 768 ?
-            <Menu /> :
-            <SubMenu />
-          }
-        </div >
-      </div>
-      <motion.div className=''
+            <div className='grow flex items-center justify-start'>
+              <motion.div className="" onClick={goHome}
+                whileTap={{
+                  scale: 0.95,
+                  transition: { duration: 0.1 }
+                }}>
+                <NOWHERE_LOGO className="h-10" />
+              </motion.div>
+            </div>
+            {width >= 768 ?
+              <Menu /> :
+              <SubMenu />
+            }
+          </div >
+        </div>
+      </motion.div>
+      <motion.div className='fixed w-full z-10'
         animate={{ y: subMenuY }}
-        transition={{ duration: 0.2 }}>
+        transition={{ duration: 0.3 }}>
         <SubMenuDropdown />
       </motion.div>
-    </motion.div>
+    </div>
   )
 }
 
--- src/Header/components/SubMenuDropdown.tsx
+++ src/Header/components/SubMenuDropdown.tsx
@@ -1,16 +1,24 @@
 import MenuButton from "./MenuButton";
 import MENU_LIST from "../../constant/MenuList";
+import { useAppSelector } from "../../hooks";
 
 const SubMenuDropdown = () => {
+  const { showSubMenuDropdown } = useAppSelector(state => state.headerState)
   const menuList: JSX.Element[] = MENU_LIST.list.map(
-    (menu) => <MenuButton key={menu.id} title={menu.title} path={menu.path}/>
+    (menu) => <MenuButton key={menu.id} title={menu.title} path={menu.path} />
   );
   return (
-    <div className="z-10 w-full flex justify-center items-center p-4 bg-black">
+    <div className={
+      showSubMenuDropdown ?
+        "z-10 w-full flex justify-center items-center p-4 bg-black"
+        :
+        "z-10 w-full flex justify-center items-center p-4 bg-black pointer-events-none"
+    }
+    >
       <div className="flex gap-4 flex-col">
         {menuList}
       </div>
-    </div>
+    </div >
   )
 }
 
