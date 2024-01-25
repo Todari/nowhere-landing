23343cb81a7e590a694e53309ce31a03ced8f244
이태훈 <rhymint@gmail.com>
2024-01-25 14:36:04 +0900

    feat: "추가 LabelCard LabelInfo handler"

--- src/Labels/LabelCard.tsx
+++ src/Labels/LabelCard.tsx
@@ -1,4 +1,7 @@
 import { twMerge } from "tailwind-merge"
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import LabelInfo from "./LabelInfo";
 
 type LabelCardProps = {
   id: number,
@@ -10,22 +13,42 @@ type LabelCardProps = {
 }
 
 const LabelCard = ({ id, name, profile, soundCloud, tracks, description }: LabelCardProps) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const infoHandler = () => {
+    setShowInfo(!showInfo)
+  }
 
   return (
-    <div className="w-full flex flex-row gap-8 rounded-3xl items-start bg-neutral-900 p-8" >
-      <div className={"flex w-full basis-1/4 aspect-square justify-center items-center rounded-full bg-contain bg-center bg-no-repeat bg-black"}
-      style={{
-        backgroundImage: `url(${profile})`
-      }}
-      onClick={() => window.open(soundCloud)}>
+    <div className="flex flex-col rounded-3xl items-start bg-neutral-900 pt-8 px-8 gap-4">
+      <div className="w-full flex flex-row gap-8 " >
+        <div className="flex w-full basis-1/4 aspect-square justify-center items-center">
+          <div className={"w-full aspect-square rounded-full bg-contain bg-center bg-no-repeat bg-black"}
+            style={{
+              backgroundImage: `url(${profile})`
+            }}
+            onClick={() => window.open(soundCloud)}></div>
+        </div>
+        <div className="basis-3/4 flex flex-col divide-y items-center justify-center">
+          <div className="text-white text-left pb-4">
+            {name}
+          </div>
+          <div className="text-stone-400 text-xs text-wrap text-left leading-relaxed pt-4">
+            {description}
+          </div>
+        </div>
       </div>
-      <div className="basis-3/4 flex flex-col divide-y items-center justify-center">
-        <div className="text-white text-left pb-4">
-          {name}
-        </div>
-        <div className="text-stone-400 text-xs text-wrap text-left leading-relaxed pt-4">
-          {description}
-        </div>
+      {showInfo ? <LabelInfo /> : null}
+      <div className="flex w-full h-8 justify-center items-center"
+        onClick={infoHandler}>
+        {showInfo ?
+
+          <FaAngleUp className="w-full h-full fill-neutral-500" />
+
+          :
+          <FaAngleDown className="w-full h-full fill-neutral-500" />
+        }
+
       </div>
     </div>
   )
---
+++ src/Labels/LabelInfo.tsx
@@ -0,0 +1,10 @@
+const LabelInfo = () => {
+
+  return(
+    <div className="flex flex-col w-full h-24">
+
+    </div>
+  )
+}
+
+export default LabelInfo
